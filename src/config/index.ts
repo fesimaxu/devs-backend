4af3adb3b505dbad3af3a5883761b54d15ab7231
Lawrence ime <lawrenceime246@gmail.com>
2023-07-04 15:13:54 +0100

    Changes made on the index.ts by Lawrence

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+const organization = [
+    {
+        id: 1,
+        name: "Decagon",
+        location: "Lekki",
+    },
+    {
+        id: 2,
+        name: "Verraki",
+        location: "Mainland",
+    },
+    {
+        id: 3,
+        name: "Dangote",
+        location: "Oshodi",
+    }
+];
